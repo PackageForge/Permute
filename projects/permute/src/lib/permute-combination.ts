e27cd8acf955e4fbb70e7ef5eba2d094b255a3df
Scott Nesin <snesin@gmail.com>
2021-09-16 14:59:36 -0500

    permuteCombination with array of arrays

--- projects/permute/src/lib/permute-combination.ts
+++ projects/permute/src/lib/permute-combination.ts
@@ -1,14 +1,18 @@
+export function permuteCombination<T>(a: T[][], callback: (p: T[]) => false | any): undefined | false
 export function permuteCombination<A, B>(a: A[], b: B[], callback: (a: A, b: B) => false | any): undefined | false
 export function permuteCombination<A, B, C>(a: A[], b: B[], c: C[], callback: (a: A, b: B, c: C) => false | any): undefined | false
 export function permuteCombination<A, B, C, D>(a: A[], b: B[], c: C[], d: D[], callback: (a: A, b: B, c: C, d: D) => false | any): undefined | false
 export function permuteCombination<A, B, C, D, E>(a: A[], b: B[], c: C[], d: D[], e: E[], callback: (a: A, b: B, c: C, d: D, e: E) => false | any): undefined | false
 export function permuteCombination(...collections: any[]) {
   const callback: (...c: any[]) => false | any = collections.pop();
+  const noSpread = collections.length === 1;
+  if (noSpread)
+    collections = collections[0];
   const count = collections.length;
   const permutation: any[] = [];
   function dive(depth: number) {
     if (depth === count) {
-      if (callback(...permutation) === false)
+      if ((noSpread ? callback(permutation.slice()) : callback(...permutation)) === false)
         return false;
     } else {
       const collection = collections[depth] as any[];
